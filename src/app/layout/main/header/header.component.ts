5c42b9a03f25a92a144b1236be8c73ea51d5ef13
marioteran56 <marioteran1@hotmail.com>
2022-11-21 09:54:01 -0600

    Categories Service Implementation for Header

--- src/app/layout/main/header/header.component.ts
+++ src/app/layout/main/header/header.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,17 @@ import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) { }
+  categories: any[] = [];
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal, private http: HttpClient, private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
-
+    this.categoriesService.getCategories().subscribe((res) => {
+      this.categories = <any[]>res.categories;
+    });
   }
 
   openUserInfo(content: any) {
     this.modalService.open(content);
   }
-
 }
---
+++ src/app/services/categories.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<any> {
+    return this.http.get<any>('assets/data/categories.json');
+  }
+}
